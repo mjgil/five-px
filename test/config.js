de7c94952e7fe6e8556943156dfdaaa418e411bd
Malcom Gilbert <malcomgilbert@gmail.com>
2013-09-07 21:25:23 -0700

    error messages when environment variable is not set

--- test/config.js
+++ test/config.js
@@ -1,4 +1,14 @@
-// Enter your consumer_key below and copy file to config.js
+// Set your environment variables to enable the tests
+var uf = require('util').format,
+    envNames = ['FIVE_PX_CONSUMER_KEY', 'FIVE_PX_USER_ID', 
+                'FIVE_PX_USER_NAME', 'FIVE_PX_USER_EMAIL'];
+
+envNames.forEach(function(envName) {
+  if (!process.env[envName]) {
+    console.error(uf('  \033[1;31m ERROR\033[0m: Please set your environment variable - %s', envName));
+  }
+});
+
 module.exports = {
   "secrets": {
     "consumer_key": process.env.FIVE_PX_CONSUMER_KEY,
